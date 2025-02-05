f185bde0ac197bc67ba2722bb200cc584134c49b
Julio Nunes Avelar <jn5.guape@gmail.com>
2025-02-05 10:47:22 -0300

    Iniciando novos testes

--- utils/run_all.py
+++ utils/run_all.py
@@ -21,17 +21,26 @@ Nota:
     O arquivo de entrada `arquivos.txt` deve conter uma URL por linha.
 """
 
+import time
 import subprocess
 
 # Caminho para o arquivo que contém a lista de URLs
-FILE_PATH = 'processadores2.txt'
+MODEL_PATH = 'models.txt'
+FILE_PATH = 'processadores3.txt'
 
 # Abrir o arquivo e ler as URLs
 with open(FILE_PATH, 'r', encoding='utf-8') as file:
     urls = file.readlines()
 
+# Abrir o arquivo e ler o nome dos modelos
+with open(MODEL_PATH, 'r', encoding='utf-8') as file:
+    models = file.readlines()
+
 # Remover qualquer espaço ou quebra de linha ao final de cada URL
 urls = [url.strip() for url in urls]
+
+# Remover qualquer espaço ou quebra de linha ao final de cada modelo
+models = [model.strip() for model in models]
 
 # Comando base
 command_base = [
@@ -42,24 +51,59 @@ command_base = [
     '',
     '-c',
     '-a',
+    '-p',
+    '',
+    '-m',
+    '',
     #'-n',
 ]
 
 # Timeout de 3 minutos (180 segundos)
 TIMEOUT_SECONDS = 720
 
-# Para cada URL na lista, executar o comando com timeout
-for url in urls:
-    # Montar o comando com a URL
-    command_base[3] = url  # Substituir a URL no comando
-    print(f"Executando: {' '.join(command_base)}")
+urls_falharam = []
+urls_sucesso = []
 
-    try:
-        # Executar o comando com timeout
-        subprocess.run(command_base, timeout=TIMEOUT_SECONDS, check=True)
-    except subprocess.TimeoutExpired:
-        print(
-            f'Comando para {url} atingiu o tempo limite de {TIMEOUT_SECONDS} segundos.'
-        )
-    except subprocess.CalledProcessError as e:
-        print(f'Erro ao executar o comando para {url}: {e}')
+relatorio = []
+
+# Para cada URL na lista, executar o comando com timeout
+for model in models:
+    tempo_inicio = time.time()
+    command_base[8] = f'{model}.json'
+    command_base[10] = model
+
+    print(f'Modelo: {model}\n\n')
+
+    for url in urls:
+        # Montar o comando com a URL
+        command_base[4] = url  # Substituir a URL no comando
+        print(f"Executando: {' '.join(command_base)}")
+
+        try:
+            # Executar o comando com timeout
+            subprocess.run(command_base, timeout=TIMEOUT_SECONDS, check=True)
+            urls_sucesso.append(url)
+        except subprocess.TimeoutExpired:
+            print(
+                f'Comando para {url} atingiu o tempo limite de {TIMEOUT_SECONDS} segundos.'
+            )
+            urls_falharam.append(url)
+        except subprocess.CalledProcessError as e:
+            print(f'Erro ao executar o comando para {url}: {e}')
+
+    tempo_fim = time.time()
+    print(f'Tempo total de execução: {tempo_fim - tempo_inicio} segundos')
+
+    relatorio.append(
+        f'Modelo: {model}\nTempo total de execução: {tempo_fim - tempo_inicio} segundos\n\n'
+    )
+
+print('\n')
+
+print(f'URLs que falharam: {urls_falharam}')
+print(f'URLs que tiveram sucesso: {urls_sucesso}')
+
+with open('relatorio.txt', 'w', encoding='utf-8') as file:
+    file.writelines(relatorio)
+
+print("Relatório salvo em 'relatorio.txt'")
